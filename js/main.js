9b583cc45e9fee46e75998208161c361b2c57a25
Mii <mutron.xiaomii@gmail.com>
2025-07-09 15:29:54 +0800

    更新PWA+點擊放大問題

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // 📁 自動釣魚遊戲主邏輯
 
-const GAME_VERSION = "2.6.0"; // 每次更新請手動更改版本號
+const GAME_VERSION = "3.0.0"; // 每次更新請手動更改版本號
 const STORAGE_KEY = "fishing-v3-backpack";
 const ownedEquipment = "owned-equipment-v2";
 const EQUIPPED_KEY = "equipped-items-v2";
